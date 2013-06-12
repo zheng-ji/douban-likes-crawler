76fc37161d3218c8bc5421900273af51a612e441
zheng-ji <407562752@qq.com>
2013-06-12 21:56:00 +0800

    modify

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from crawler import Crawler
 
 if __name__ == "__main__":
     #用户名
-    usrnames = ["cloudwu","fenng"]
+    usrnames = ["laiyonghao","fenng"]
     for name in usrnames:
         cwl = Crawler(name)
         cwl.start()
